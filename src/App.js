aa90a0d0ac7617e7cc757e356e66811f479982ed
Victor Roşca <49694231+roscavictor@users.noreply.github.com>
2021-02-23 08:59:13 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Switch, Route, BrowserRouter as Router, Redirect } from 'react-router-dom';
+import logo from './logo.svg';
+import Login from './components/Login';
+import Register from './components/Register';
+import Profile from './components/Profile';
+import MainPage from './components/MainPage';
+
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+          <div>
+          <Switch>
+            {/* login */}
+            <Route exact path="/" component={Login}></Route>
+            <Route exact path="/Register" component={Register}></Route>      
+            <Route exact path="/Profile" component={Profile}></Route>    
+            <Route exact path="/MainPage" component={MainPage}></Route>      
+          </Switch>        
+        </div>
+      </Router>
+  );
+}
+
+export default App;
